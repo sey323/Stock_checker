0c96ba3e01014fb67c2b0d0b705c35a14c6cd5c9
sey323 <sey.march23@gmail.com>
2018-04-16 22:19:13 +0900

    src/price_call.jsの削除．./src/slack.jsに機能の集約．main.jsの大幅な修正

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// Cronの読み込み
-var CronJob = require('cron').CronJob;
-// 自作ファイルの読み込み
-var pc = require('./src/price_call.js');
-var jsonUtil = require('./src/json_util.js');
-
-var json = new jsonUtil.JsonUtil('config.json');
-var stock = json.get_stock();
-var slack_param = json.get_slack();
-
-//Slackコールの呼び出し
 var slack = require('./src/slack.js');
-var slack = new slack.Slack( slack_param.token );
 
-new CronJob({
-    cronTime:'00,30 8-16 * * 0-7',
-    onTick: function() {
-      var companies = stock.companies;
-      pc.price_call( companies );
-    },
-    start: true,
-    timeZone: 'Asia/Tokyo'
-});
+var slacker = new slack.Slack();
--- src/slack.js
+++ src/slack.js
@@ -1,26 +1,54 @@
+var Botkit = require('botkit');
+// Cronの読み込み
+var CronJob = require('cron').CronJob;
+
+//独自ライブラリの呼び出し
+var jsonUtil = require('./json_util.js')
+var sp = require('./stock_price');
 
 //slackモジュール
-exports.Slack = function( token ){
-  var Botkit = require('botkit');
-
+exports.Slack = function(){
   const controller = Botkit.slackbot({
     debug: false
   });
 
+  //キーの取得
+  var json = new jsonUtil.JsonUtil('config.json');
+  var slack_param = json.get_slack();
+  var stock = json.get_stock();
+  var companies = stock.companies;
+
   // 初期化
   controller.spawn({
-      token: token
-  }).startRTM(function(err){
-      if (err) {
-          throw new Error(err);
-      }
+    token: slack_param.token
+  }).startRTM(function(err, bot, payload) {
+    new CronJob({
+      //cronTime: '* * * * *',
+      cronTime:'00,30 8-16 * * 0-7',
+      onTick: function() {
+
+        // 会社の数だけ繰り返す．
+        companies.forEach( function( company ){
+          var getVal = function( result ){
+            var message = sp.slack_formatting( result );
+            bot.say( {
+              text:message,
+              channel: 'virtual-currency'
+            } );
+          }
+          var message = sp.getNowprice( company.name , getVal );
+        });
+      },
+      start: true,
+      timeZone: 'Asia/Tokyo'
+    });
   });
 
+  // 会社の名前を入力したら，その株価を返信
   controller.hears('(.*)',['direct_message'],function( bot , message) {
-    bot.reply( message , message.text + 'の株価を検索してるぜ！' );
-
     var sp = require('./stock_price');
     var getVal = function( company ){
+      bot.reply( message , message.text + 'の株価を検索してるぜ！' );
       var value = sp.slack_formatting( company );
       // 存在しない企業の時
       if( value == null ){
@@ -29,21 +57,5 @@ exports.Slack = function( token ){
         bot.reply( message , value );
       }
     }
-    var tmp = sp.getNowprice( message.text , getVal );
   });
-
-  // チャンネルに定期発信
-  this.say_message = function( message ){
-    controller.spawn({
-      token: token
-    }).startRTM(function(err, bot, payload) {
-      // チャンネル等を指定
-      bot.say({
-        channel: 'virtual-currency',
-        text: message,
-        username: 'rocket'
-      });
-      // --
-    });
-  }
 }
