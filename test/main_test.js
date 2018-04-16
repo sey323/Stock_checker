7a8e10d4634175c282134a8ca2566aee5235a7fe
sey323 <sey.march23@gmail.com>
2018-04-16 21:46:57 +0900

    node_moduleがなくてもコンテナ実行確認．debug.jsをtestフォルダに移動．

--- test/main_test.js
+++ test/main_test.js
@@ -1,19 +1,19 @@
 // Cronの読み込み
 var CronJob = require('cron').CronJob;
 // 自作ファイルの読み込み
-var pc = require('./src/price_call.js');
-var jsonUtil = require('./src/json_util.js')
+var pc = require('../src/price_call.js');
+var jsonUtil = require('../src/json_util.js')
 
 var json = new jsonUtil.JsonUtil('config.json');
 var stock = json.get_stock();
 var slack_param = json.get_slack();
 
 //Slackコールの呼び出し
-var slack = require('./src/slack.js');
+var slack = require('../src/slack.js');
 var slack = new slack.Slack( slack_param.token );
 
 new CronJob({
-    cronTime: '* * * * *',
+    cronTime: '47 * * * *',
     onTick: function() {
       var companies = stock.companies;
       pc.price_call( companies );
